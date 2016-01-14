18529d4624c63a569328b801d5261bc5c0af6efa
TEAM-SA-SOFT <xray818181@gmail.com>
2016-01-14 07:05:35 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,29 +221,15 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {111057732,114779183(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[SA_BOT for admin
+    @Xray4:sudo
+    and
+    Developer
+    @hero
+    
 ]],
     help_text_realm = [[
 Realm Commands:
